2454c4a46710be0d3b33c59f75f1a7aff2b58f50
gingaminga <gingahae@gmail.com>
2023-03-24 18:31:56 +0900

    ➕ Init: react-query 추가 및 초기 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,18 @@
+import queryClient from "@/queries/query-client";
 import store from "@/store";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Provider } from "react-redux";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+        <ReactQueryDevtools position="bottom-left" />
+      </QueryClientProvider>
     </Provider>
   );
 }
