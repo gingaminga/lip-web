0874b1d18b45dbed451a1a097361e320cc97254d
gingaminga <gingahae@gmail.com>
2023-04-10 17:39:15 +0900

    ♻️ Refactor: 페이지 공통 부분 위치 변경
    theme toggle 부분은 공통이기 때문에 _app.tsx 로 위치 변경

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import ActivityMain from "@/components/ActivityMain";
-import ThemeToggle from "@/components/ThemeToggle";
 import Head from "next/head";
 
 export default function Home() {
@@ -8,9 +7,6 @@ export default function Home() {
       <Head>
         <title>Sample</title>
       </Head>
-      <div className="fixed right-5 top-5">
-        <ThemeToggle />
-      </div>
       <div className="hero h-screen">
         <div className="hero-content flex-col text-center">
           <ActivityMain />
