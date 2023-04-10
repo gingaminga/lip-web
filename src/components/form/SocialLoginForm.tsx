11c4842e179cf13a67ee15852b0190e3234f6f7f
gingaminga <gingahae@gmail.com>
2023-04-11 00:39:18 +0900

    💄 Fix: UI 수정
    SocialLoginForm 컴포넌트는 card 형식으로 처리

--- src/components/form/SocialLoginForm.tsx
+++ src/components/form/SocialLoginForm.tsx
@@ -6,15 +6,17 @@ import { SOCIAL_LOGIN_CONFIG } from "@/utils/social-login-config";
  */
 export default function SocialLoginForm() {
   return (
-    <>
-      {SOCIAL_LOGIN_CONFIG.map((config) => {
-        const { className, LogoComponent, text } = config;
-        return (
-          <LogoButton buttonClassName={className} key={`logo-button-${text}`} value={text}>
-            <LogoComponent className="w-7 h-7" />
-          </LogoButton>
-        );
-      })}
-    </>
+    <div className="card shadow-2xl max-w-full w-1/2">
+      <div className="card-body justify-center">
+        {SOCIAL_LOGIN_CONFIG.map((config) => {
+          const { className, LogoComponent, text } = config;
+          return (
+            <LogoButton buttonClassName={className} key={`logo-button-${text}`} value={text}>
+              <LogoComponent className="w-7 h-7" />
+            </LogoButton>
+          );
+        })}
+      </div>
+    </div>
   );
 }
