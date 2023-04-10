c62dfbe9bd6c278e9574821763e24f2f973da429
gingaminga <gingahae@gmail.com>
2023-04-10 23:20:59 +0900

    💄 Feat: LogoButton component을 storybook에 추가

---
+++ src/stories/common/atoms/LogoButton.stories.tsx
@@ -0,0 +1,40 @@
+import LogoButton from "@/components/common/atoms/LogoButton";
+import MockProvider from "@/stories/MockProvider";
+import { SOCIAL_LOGIN_CONFIG } from "@/utils/social-login-config";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+export default {
+  title: "Atoms/Button/LogoButton",
+  component: LogoButton,
+  decorators: [(story) => <MockProvider>{story()}</MockProvider>],
+} as ComponentMeta<typeof LogoButton>;
+
+const Template: ComponentStory<typeof LogoButton> = (args) => <LogoButton {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  value: "Logo Button Text",
+};
+
+const [KAKAO_CONFIG, NAVER_CONFIG, GOOGLE_CONFIG] = SOCIAL_LOGIN_CONFIG;
+
+export const Kakao = Template.bind({});
+Kakao.args = {
+  buttonClassName: KAKAO_CONFIG.className,
+  children: <KAKAO_CONFIG.LogoComponent className="w-7 h-7" />,
+  value: KAKAO_CONFIG.text,
+};
+
+export const Naver = Template.bind({});
+Naver.args = {
+  buttonClassName: NAVER_CONFIG.className,
+  children: <NAVER_CONFIG.LogoComponent className="w-7 h-7" />,
+  value: NAVER_CONFIG.text,
+};
+
+export const Google = Template.bind({});
+Google.args = {
+  buttonClassName: GOOGLE_CONFIG.className,
+  children: <GOOGLE_CONFIG.LogoComponent className="w-7 h-7" />,
+  value: GOOGLE_CONFIG.text,
+};
