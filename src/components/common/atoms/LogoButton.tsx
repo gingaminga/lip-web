dc56f0a31be44811b2896d45a372b2bfafa1214f
gingaminga <gingahae@gmail.com>
2023-04-23 23:13:51 +0900

    ✨ Feat: LogoButton component 클릭 이벤트 추가

--- src/components/common/atoms/LogoButton.tsx
+++ src/components/common/atoms/LogoButton.tsx
@@ -1,15 +1,23 @@
+import { MouseEventHandler } from "react";
+
 interface ILogoButton {
   buttonClassName?: string; // 버튼 css
   children: JSX.Element; // 로고 컴포넌트
+  onClick?: MouseEventHandler<HTMLButtonElement>;
   value: string; // 버튼 텍스트
 }
 
 /**
  * @description 로고가 있는 버튼 컴포넌트
  */
-export default function LogoButton({ buttonClassName = "btn btn-block gap-2.5", children, value }: ILogoButton) {
+export default function LogoButton({
+  buttonClassName = "btn btn-block gap-2.5",
+  children,
+  onClick,
+  value,
+}: ILogoButton) {
   return (
-    <button className={`${buttonClassName} max-sm:btn-circle`} type="button">
+    <button className={`${buttonClassName} max-sm:btn-circle`} onClick={onClick} type="button">
       {children}
       <span className="max-sm:hidden">{value}</span>
     </button>
