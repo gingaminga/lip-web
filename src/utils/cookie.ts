b7e300e69c15d69565d79aa6ecb114cdd287b1c6
gingaminga <gingahae@gmail.com>
2023-05-02 17:28:17 +0900

    ✨ Feat: 브라우저 쿠키 기능 추가
    js-cookie 설치
    cookie 유틸 추가

---
+++ src/utils/cookie.ts
@@ -0,0 +1,40 @@
+import Cookies, { CookieAttributes } from "js-cookie";
+
+export default class Cookie {
+  /**
+   * @description 쿠키 설정하기
+   * @param name
+   * @param value
+   * @param options
+   */
+  static setCookie(name: string, value: string, options?: CookieAttributes) {
+    const { expires = 1000 * 24 * 60 * 60, ...rest } = options || {};
+
+    const newOptions = {
+      ...rest,
+      expires,
+    };
+
+    const response = Cookies.set(name, value, newOptions);
+
+    return !!response;
+  }
+
+  /**
+   * @description 쿠키 가져오기
+   * @param name
+   */
+  static getCookie(name: string) {
+    return Cookies.get(name) || "";
+  }
+
+  /**
+   * @description 쿠키 삭제하기
+   * @param name
+   */
+  static removeCookie(name: string) {
+    Cookies.remove(name);
+
+    return true;
+  }
+}
