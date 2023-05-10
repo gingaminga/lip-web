bbc6b12779be392a379e30708f3f5391b6ef2f7b
gingaminga <gingahae@gmail.com>
2023-05-11 00:01:26 +0900

    ♻️ Refactor: 고정 컴포넌트 제거
    테마 토글 처리하는 컴포넌트를 고정 처리에서 제거 (nav bar에 넣을 예정)

--- src/components/common/organisms/WrapperCommonComponent.tsx
+++ src/components/common/organisms/WrapperCommonComponent.tsx
@@ -1,20 +1,8 @@
 import LoadingOverlay from "@/components/common/organisms/LoadingOverlay";
-import ThemeToggle from "@/components/ThemeToggle";
 import useRegisterToken from "@/hooks/useRegisterToken";
 import useVisibleLayout from "@/hooks/useVisibleLayout";
 import { AppProps } from "next/app";
 import { useMemo } from "react";
-
-/**
- * @description 고정 컴포넌트
- */
-function FixedComponent() {
-  return (
-    <div className="fixed right-5 top-5">
-      <ThemeToggle />
-    </div>
-  );
-}
 
 /**
  * @description 공통적으로 사용될 컴포넌트들을 묶어놓은 컴포넌트
@@ -31,10 +19,5 @@ export default function WrapperCommonComponent({ Component, pageProps }: AppProp
     return <Component {...pageProps} />;
   }, [Component, isLoadingReissueToken, isRenderLoadingOverlay, pageProps]);
 
-  return (
-    <>
-      <FixedComponent />
-      {ComponentView}
-    </>
-  );
+  return ComponentView;
 }
