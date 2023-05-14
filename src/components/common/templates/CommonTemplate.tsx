941d5cc9b67ad611a82918e636f4e60e8ffe0275
gingaminga <gingahae@gmail.com>
2023-05-14 16:55:33 +0900

    💄 Style: UI 여백 값 변경

--- src/components/common/templates/CommonTemplate.tsx
+++ src/components/common/templates/CommonTemplate.tsx
@@ -12,7 +12,7 @@ export default function CommonTemplate({ children, isLogin }: ICommonTemplate) {
       <header className="top-0">
         <NavigationBar isLogin={isLogin} />
       </header>
-      <div className="hero h-[calc(100%-4rem)] w-full py-5 px-10 max-sm:p-0">{children}</div>
+      <div className="hero h-[calc(100%-4rem)] w-full py-10 px-20 max-sm:p-0">{children}</div>
     </div>
   );
 }
