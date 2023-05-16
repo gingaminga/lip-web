c027f4467d6530044d18e3ad465aa9c634327d1c
gingaminga <gingahae@gmail.com>
2023-05-16 18:01:54 +0900

    🎨 Style: mobile view에서의 색상 처리
    mobile view에서는 색상을 gnb와 같게 처리

--- src/components/common/templates/MainTemplate.tsx
+++ src/components/common/templates/MainTemplate.tsx
@@ -9,7 +9,7 @@ interface IMainTemplate extends ICommonProps {
 export default function MainTemplate({ children, isLogin }: IMainTemplate) {
   return (
     <CommonTemplate isLogin={isLogin}>
-      <div className="flex justify-start h-full w-full max-w-full shadow-xl rounded-2xl bg-base-200 max-sm:rounded-none max-sm:flex-col-reverse">
+      <div className="flex justify-start h-full w-full max-w-full shadow-xl rounded-2xl bg-base-200 max-sm:bg-base-100 max-sm:rounded-none max-sm:flex-col-reverse">
         <div className="hero-content basis-5 w-full h-full rounded-l-[inherit] bg-base-300">메뉴리스트</div>
         <div className="hero-content w-full h-full max-w-full">{children}</div>
       </div>
