b4ca1653c4c889cd20b96d274890ea61783f74fc
gingaminga <gingahae@gmail.com>
2023-05-16 18:02:29 +0900

    ✨ Feat: 년월일 가져오는 함수 구현

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,8 @@
+import dayjs from "dayjs";
+
 export const timeout = (ms = 500) =>
   new Promise((resolve) => {
     setTimeout(resolve, ms);
   });
+
+export const getYYYYMMDD = (date = new Date()) => dayjs(date).format("YYYYMMDD");
