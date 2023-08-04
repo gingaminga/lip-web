6adccf31d8f17e9a7502b07c454a57749874d860
gingaminga <gingahae@gmail.com>
2023-08-04 23:56:13 +0900

    ⚡️ Perf: ios input box auto zoom 처리
    ios에서 input box가 자동으로 zoom이 되는 현상을 방지하고자 viewport 속성 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,12 +6,16 @@ import queryClient from "@/utils/query-client";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { Provider } from "react-redux";
 
 export default function App(props: AppProps) {
   return (
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        </Head>
         <WrapperCommonComponent {...props} />
         <ReactQueryDevtools position="bottom-left" />
       </QueryClientProvider>
